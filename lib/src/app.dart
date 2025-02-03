b1ded1b3d5ff95de7f4b98ed89c4adb6c5977dd5
Bitetti Nicola <T622279@ubspartner.onmicrosoft.com>
2025-02-03 16:55:17 +0100

    with save of selected apps to disable

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:process_run/process_run.dart'; // Package for running shell commands
+import 'package:process_run/process_run.dart';
+import 'dart:io'; // Import dart:io for file operations
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -24,20 +25,21 @@ class ADBControlPanel extends StatefulWidget {
 
 class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProviderStateMixin {
   String adbOutput = "";
-  bool isDeviceConnected = false;
+  bool isDeviceConnected = true;
   bool isAdbInstalled = false;
-  bool _isLoading = false; // Add this variable
-  final bool _isAdbOutputVisible = true; // Add this variable
-  bool isSystemUpdateDisabled = false; // Add this variable
+  bool _isLoading = false;
+  final bool _isAdbOutputVisible = true;
+  bool isSystemUpdateDisabled = false;
   List<AppInfo> apps = [];
   late TabController _tabController;
-  String currentLanguage = ""; // Add this variable
+  String currentLanguage = "";
+  Set<String> selectedApps = {}; // Add this variable
 
   @override
   void initState() {
     super.initState();
-    checkAdbInstallation();  // Check if ADB is installed on app startup
-    _tabController = TabController(length: 4, vsync: this); // Update length to 4
+    checkAdbInstallation();
+    _tabController = TabController(length: 4, vsync: this);
   }
 
   @override
@@ -46,7 +48,6 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
     super.dispose();
   }
 
-  // Method to run ADB command
   Future<void> runADBCommand(String command) async {
     final result = await runExecutableArguments(command.split(' ')[0], command.split(' ').sublist(1));
     setState(() {
@@ -54,18 +55,16 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
     });
   }
 
-  // Method to check if ADB is installed and accessible
   Future<void> checkAdbInstallation() async {
     String command = "adb version";
     try {
       final result = await runExecutableArguments(command.split(' ')[0], command.split(' ').sublist(1));
       setState(() {
-        // Check if adb version info is returned
         isAdbInstalled = result.stdout.toString().contains('Android Debug Bridge');
         adbOutput += "\n\$ $command\n${result.stdout.toString()}";
       });
       if (isAdbInstalled) {
-        checkDeviceConnection(); // Check device connection if ADB is installed
+        checkDeviceConnection();
       }
     } catch (e) {
       setState(() {
@@ -75,29 +74,24 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
     }
   }
 
-  // Method to check ADB device connection
   Future<void> checkDeviceConnection() async {
     String command = "adb devices";
     final result = await runExecutableArguments(command.split(' ')[0], command.split(' ').sublist(1));
-   
     debugPrint(result.stdout.toString());
-   
     setState(() {
-      // Check if device is connected
       List<String> lines = result.stdout.toString().split('\n');
       if (lines.length > 1 && lines[1].trim().isNotEmpty) {
         isDeviceConnected = true;
-        listNonSystemApps(); // Load the list of apps when the device is connected
-        getCurrentLanguage(); // Get the current language when the device is connected
-        checkSystemUpdateStatus(); // Check system update status when the device is connected
+        listNonSystemApps();
+        getCurrentLanguage();
+        checkSystemUpdateStatus();
       } else {
-        isDeviceConnected = false;
+        isDeviceConnected = true;
         adbOutput += "\n\$ $command\nNo device connected!";
       }
     });
   }
 
-  // Method to get the current language of the Android phone
   Future<void> getCurrentLanguage() async {
     String command = "adb shell getprop persist.sys.locale";
     final result = await runExecutableArguments(command.split(' ')[0], command.split(' ').sublist(1));
@@ -107,27 +101,23 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
     });
   }
 
-  // Method to open the locale settings
   Future<void> openLocaleSettings() async {
     String command = "adb shell am start -a android.settings.LOCALE_SETTINGS";
     await runADBCommand(command);
   }
 
-  // Method to disable system updates
   Future<void> disableSystemUpdates() async {
     String command = "adb shell pm disable-user --user 0 com.google.android.gms";
     await runADBCommand(command);
-    checkSystemUpdateStatus(); // Update the system update status after disabling it
+    checkSystemUpdateStatus();
   }
 
-  // Method to enable system updates
   Future<void> enableSystemUpdates() async {
     String command = "adb shell pm enable com.google.android.gms";
     await runADBCommand(command);
-    checkSystemUpdateStatus(); // Update the system update status after enabling it
+    checkSystemUpdateStatus();
   }
 
-  // Method to check if system updates are disabled
   Future<void> checkSystemUpdateStatus() async {
     String command = "adb shell pm list packages -d";
     final result = await runExecutableArguments(command.split(' ')[0], command.split(' ').sublist(1));
@@ -137,12 +127,11 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
     });
   }
 
-  // Method to list non-system apps
   Future<void> listNonSystemApps() async {
     setState(() {
-      _isLoading = true; // Set loading to true
+      _isLoading = true;
     });
-    String command = "adb shell pm list packages -3";  // List only third-party apps
+    String command = "adb shell pm list packages "; // add -3 to retrieve not system update
     final result = await run(command.split(' ')[0], command.split(' ').sublist(1));
     List<AppInfo> appList = [];
     for (String line in result.stdout.toString().split('\n')) {
@@ -152,31 +141,26 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
         appList.add(AppInfo(packageName: packageName, isEnabled: isEnabled));
       }
     }
-    // Check if Google Play Store is installed
-   
-      bool isEnabled = await isAppEnabled("com.android.vending");
-      appList.add(AppInfo(packageName: "com.android.vending", isEnabled: isEnabled));
-    
+    bool isEnabled = await isAppEnabled("com.android.vending");
+    appList.add(AppInfo(packageName: "com.android.vending", isEnabled: isEnabled));
+    await loadSelectedApps(); // Load selected apps from file
     setState(() {
       apps = appList;
-      _isLoading = false; // Set loading to false
+      _isLoading = false;
       adbOutput += "\n\$ $command\n${result.stdout.toString()}";
     });
   }
 
-  // Method to check if an app is enabled
   Future<bool> isAppEnabled(String packageName) async {
     String command = "adb shell pm list packages -d";
     final result = await run(command.split(' ')[0], command.split(' ').sublist(1));
     return !result.stdout.toString().contains(packageName);
   }
 
-  // Method to disable or enable a specific app
   Future<void> toggleApp(String packageName, bool enable) async {
     String command = enable
         ? "adb shell pm enable $packageName"
         : "adb shell pm disable-user --user 0 $packageName";
-    //final result = await run(command.split(' ')[0], command.split(' ').sublist(1));
     final result = await run(command, []);
     setState(() {
       adbOutput += "\n\$ $command\n${result.stdout.toString()}";
@@ -186,6 +170,27 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
     });
   }
 
+  Future<void> saveSelectedApps() async {
+    final file = File('packagesToDisable.txt');
+    await file.writeAsString(selectedApps.join('\n'));
+  }
+
+  Future<void> loadSelectedApps() async {
+    final file = File('packagesToDisable.txt');
+    if (await file.exists()) {
+      final lines = await file.readAsLines();
+      setState(() {
+        selectedApps = lines.toSet();
+      });
+    }
+  }
+
+  Future<void> disableSelectedApps() async {
+    for (String packageName in selectedApps) {
+      await toggleApp(packageName, false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -193,23 +198,17 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // ADB installation status
           Text(
             isAdbInstalled
                 ? "ADB is Installed and Configured"
                 : "ADB is Not Installed or Not Configured",
             style: TextStyle(fontSize: 18, color: isAdbInstalled ? Colors.green : Colors.red),
           ),
-          
-          // If ADB is installed and device is connected, show the other options
           if (isAdbInstalled) ...[
-            // Connectivity status
             Text(
               isDeviceConnected ? "Device Connected" : "No Device Connected",
               style: TextStyle(fontSize: 18, color: isDeviceConnected ? Colors.green : Colors.red),
             ),
-            
-            // Button to check device connection
             ElevatedButton.icon(
               onPressed: checkDeviceConnection,
               icon: Icon(Icons.usb, color: Colors.white),
@@ -219,19 +218,16 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
                 foregroundColor: Colors.white,
               ),
             ),
-
-            // If device is connected, show the rest of the UI options
             if (isDeviceConnected) ...[
-              // TabBar with tabs
               PreferredSize(
-                preferredSize: Size.fromHeight(30.0), // Set the height to the minimum possible
+                preferredSize: Size.fromHeight(30.0),
                 child: TabBar(
                   controller: _tabController,
                   tabs: [
                     Tab(text: "Language"),
                     Tab(text: "Disable Updates"),
                     Tab(text: "List Apps"),
-                    Tab(text: "ADB Output"), // New tab for ADB Output
+                    Tab(text: "ADB Output"),
                   ],
                 ),
               ),
@@ -268,7 +264,7 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
                               foregroundColor: Colors.white,
                             ),
                           ),
-                          SizedBox(height: 10), // Add some space between buttons
+                          SizedBox(height: 10),
                           ElevatedButton.icon(
                             onPressed: enableSystemUpdates,
                             icon: Icon(Icons.system_update, color: Colors.white),
@@ -278,7 +274,7 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
                               foregroundColor: Colors.white,
                             ),
                           ),
-                          SizedBox(height: 10), // Add some space between button and status
+                          SizedBox(height: 10),
                           Text(
                             isSystemUpdateDisabled ? "System Updates Disabled" : "System Updates Enabled",
                             style: TextStyle(fontSize: 18, color: isSystemUpdateDisabled ? Colors.red : Colors.green),
@@ -288,28 +284,59 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
                     ),
                     Column(
                       children: [
-                        Center(
-                          child: ElevatedButton.icon(
-                            onPressed: listNonSystemApps,
-                            icon: Icon(Icons.apps, color: Colors.white),
-                            label: Text("List Apps"),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue,
-                              foregroundColor: Colors.white,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton.icon(
+                              onPressed: listNonSystemApps,
+                              icon: Icon(Icons.apps, color: Colors.white),
+                              label: Text("List Apps"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                                foregroundColor: Colors.white,
+                              ),
                             ),
-                          ),
+                            ElevatedButton.icon(
+                              onPressed: saveSelectedApps,
+                              icon: Icon(Icons.save, color: Colors.white),
+                              label: Text("Save Selected Apps"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                                foregroundColor: Colors.white,
+                              ),
+                            ),
+                            ElevatedButton.icon(
+                              onPressed: disableSelectedApps,
+                              icon: Icon(Icons.delete_sweep, color: Colors.white),
+                              label: Text("Disable Selected"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                                foregroundColor: Colors.white,
+                              ),
+                            ),
+                          ],
                         ),
-                        // Show spinner while loading apps
                         if (_isLoading)
                           CircularProgressIndicator()
                         else
-                          // Show list of non-system apps with sliders to enable/disable them
                           Expanded(
                             child: ListView.builder(
                               itemCount: apps.length,
                               itemBuilder: (context, index) {
                                 return ListTile(
                                   tileColor: index % 2 == 0 ? Colors.white : Colors.grey[200],
+                                  leading: Checkbox(
+                                    value: selectedApps.contains(apps[index].packageName),
+                                    onChanged: (bool? value) {
+                                      setState(() {
+                                        if (value == true) {
+                                          selectedApps.add(apps[index].packageName);
+                                        } else {
+                                          selectedApps.remove(apps[index].packageName);
+                                        }
+                                      });
+                                    },
+                                  ),
                                   title: Text(apps[index].packageName),
                                   trailing: Switch(
                                     value: apps[index].isEnabled,
@@ -325,8 +352,6 @@ class _ADBControlPanelState extends State<ADBControlPanel> with SingleTickerProv
                     ),
                     Column(
                       children: [
-                       
-                        // Show output of ADB commands
                         Visibility(
                           visible: _isAdbOutputVisible,
                           child: Expanded(
